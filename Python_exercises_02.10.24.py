839943c7bab5c86c391f1981a9f525fcb0d3cf2d
techminds <aztechminds@gmail.com>
2024-10-03 18:45:11 +0200

    Modified with more comment

--- Python_exercises_02.10.24.py
+++ Python_exercises_02.10.24.py
@@ -13,6 +13,7 @@ else: # if it is not equal to zero and not greater than zero, then it is a negat
 
 
 # %% Part two: using for-loop to count numbers from one to ten.
+print ("Now I am going to count from 1 to 10")
 for numbers in range(1 , 11):
     print (numbers)
 
@@ -20,12 +21,13 @@ for numbers in range(1 , 11):
 # %% Part three: using while-loop to check even or odd numbers.
 
 # ask the user to give an input
+print("Let's find out, if I can figure out Odd and Even numbers!")
 zahl = int(input("Please enter a number: "))
 # check if the result of given number divided by two is zero, then print... 
 # when the given number divided by two is not zero, then it ask the user to type another number
 while zahl % 2 != 0:
-    zahl = int(input("The number is odd, please enter an even number: "))
-print(f"Congrats, you have entered an even number.")
+    zahl = int(input("it is a odd number, enter an even number: "))
+print(f"Wow, I can tell now, you have entered an even number.")
 
 
 # %% Part four: using def and return to calculate the sum of squares
@@ -34,15 +36,19 @@ def sum_of_squares (a, b):
     return a**2 + b**2
 
 # Getting user input for a and b
+print("Let's try some mathematic calculation!")
+print("What about sum of squares!? Let's try it!!!")
 a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
 
 result = sum_of_squares(a, b)
 print(f"The sum of squares is {result}")
+print ("It is fun!")
 
 
 # %% Part 5: Extra FizzBuzz
-
+print("Let's try something more funny!!")
+print("It is called 'FizzBuzz'")
 for zahl in range(1, 100):
     if zahl % 3 == 0 and zahl % 5 == 0:
         print("FizzBuzz")
@@ -53,5 +59,6 @@ for zahl in range(1, 100):
     else: 
         print (zahl)
 
-print ("\nThank for using this program :)")
+print ("\nThank for playing along :)")
+print ("I hope you had fun too!")
 # %%
